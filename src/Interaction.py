57ae5e5300facdfe3de7661ff40e03b24f28199d
cmartin-3360 <casseymartinff@gmail.com>
2026-03-31 15:05:24 -0400

    Ajout fichier pour interagir avec utilisateur

---
+++ src/Interaction.py
@@ -0,0 +1,7 @@
+import os
+
+class InteractionEnvironment:
+    def exploring():
+        # linux: Example output on Linux: '/home/username'
+        # Example output on Windows: 'C:\\Users\\username'
+        home_dir = os.path.expanduser('~')
